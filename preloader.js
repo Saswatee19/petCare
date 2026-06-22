c3c49a0eff3b9ed7a87efb4eb959ca538d8de180
19sas <19.saswatee@gmail.com>
2026-06-22 09:15:49 +0530

    preloader files added
    preloader alignment

--- preloader.js
+++ preloader.js
@@ -1,16 +1,3 @@
-let hamburger_icon = document.querySelector(".hamburger-icon");
-let close_icon = document.querySelector(".close-icon");
-let hamburger_text = document.querySelector(".hamburger-text");
-
-hamburger_icon.addEventListener('click', ()=>{
-    hamburger_text.classList.add('open');
-    hamburger_icon.classList.add('fade_out');
-})
-close_icon.addEventListener('click', () => {
-    hamburger_text.classList.remove('open');
-    hamburger_icon.classList.remove('fade_out');
-});
-
 let bird = document.querySelector(".bird");
 let dog = document.querySelector(".dog");
 let cat = document.querySelector(".cat");
@@ -30,6 +17,7 @@ let tm3 =setTimeout(()=>{
 let tm4 = setTimeout(()=>{
     cat.style.display = "flex"
 }, 1000);
+
 
 let preloader = document.querySelector(".preloader");
 
